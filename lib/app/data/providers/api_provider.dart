943d1c50fae9b27a83f843547958f840bbb24c8b
bantrungrong <141816912+bantrungrong@users.noreply.github.com>
2024-05-02 22:54:33 +0700

    update ngày 5 tháng 2

--- lib/app/data/providers/api_provider.dart
+++
@@ -1,5 +0,0 @@
-class API{
-  static const hostConnect = "http://192.168.1.5/api_drink_app/";
-  static const signUp = "$hostConnect/signup.php";
-  static const validateEmail = "$hostConnect/validate.php";
-}
